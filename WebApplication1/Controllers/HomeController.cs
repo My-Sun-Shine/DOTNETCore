051a064efebaaef94c26cdfbd33119868c214ebe
My-Sun-Shine <1399237176@qq.com>
2020-05-12 21:59:45 +0800

    002：依赖注入的Demo
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController: Controller
+    {
+        public HomeController(IClock clock)
+        {
+
+        }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -7,15 +7,36 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication1.Services;
 
 namespace WebApplication1
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        /// 在该方法中注册服务
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
+            #region 注册系统服务
+            //services.AddMvc();//使用该方法太庞大
+            //对控制器、API 相关功能和视图（而不是页）的支持
+            services.AddControllersWithViews();
+            //只对控制器，做API的时候
+            //services.AddControllers();
+            #endregion
+
+            #region 注册自定义服务
+
+            //单例模式注册服务
+            //每当请求IClock接口的时候，返回ChinaClock的实例
+            services.AddSingleton<IClock, ChinaClock>();
+            //HomeController使用IClock接口接收，解耦合
+            //services.AddSingleton<IClock, UTCClock>();
+            #endregion
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
