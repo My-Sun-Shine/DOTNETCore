8a7f4ff00115159a1df61a8ff3e80330ef948c06
My-Sun-Shine <1399237176@qq.com>
2020-05-13 23:36:24 +0800

    011：Add.cshtml视图
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

--- WebApplication1/Models/Department.cs
+++ WebApplication1/Models/Department.cs
@@ -1,13 +1,18 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Department
     {
         public int Id { get; set; }
-
+        
+        [Display(Name="部门名称")]
         public string Name { get; set; }
 
+        [Display(Name = "部门地址")]
         public string Location { get; set; }
 
+        [Display(Name = "部门人数")]
         public int EmployeeCount { get; set; }
     }
 }
