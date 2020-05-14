55a7f06290b93af3c4b0cfc7410dc2020082e751
My-Sun-Shine <1399237176@qq.com>
2020-05-14 22:07:45 +0800

    013：清除配置信息，添加新的配置文件
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -18,6 +18,15 @@ namespace WebApplication1
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                /*
+                 * 清除配置信息，添加新的配置文件
+                /*
+                .ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder.Sources.Clear();//清除所有的配置信息
+                    configBuilder.AddJsonFile("nick.json");//添加新的配置文件，只有nick.json起作用
+                })
+                */
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
